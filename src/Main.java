538a7ad674d0407ac0507c70b833d3197b03b911
WalterLimaViana <walter.lima.viana@gmail.com>
2022-09-20 21:57:04 -0300

    Criado o método Calculadora

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        // Calculadora
+        System.out.println("Exercicio Calculadora");
+        Calculadora.soma(4,6);
+        Calculadora.subtracao(7,9);
+        Calculadora.divisao(92,8);
+        Calculadora.multiplicacao(7,5);
     }
 }
